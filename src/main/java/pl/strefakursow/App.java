d40eeb36c2e3d9299ec78d4221f0a4135a72d3ef
wrzosdev <wrzosdev@gmail.com>
2018-07-25 19:52:40 +0200

    Podpięcie Lomboka

--- src/main/java/pl/strefakursow/App.java
+++ src/main/java/pl/strefakursow/App.java
@@ -1,8 +1,11 @@
 package pl.strefakursow;
 
+import lombok.AllArgsConstructor;
+
 /**
  * Hello world!
  */
+@AllArgsConstructor
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
