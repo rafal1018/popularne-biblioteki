0db863ba644765b0b01b014b117e0c4db91c8031
wrzosdev <wrzosdev@gmail.com>
2018-07-30 15:26:52 +0200

    Podpięcie SimpleXml

--- src/main/java/pl/strefakursow/App.java
+++ src/main/java/pl/strefakursow/App.java
@@ -2,6 +2,7 @@ package pl.strefakursow;
 
 import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
+import org.simpleframework.xml.Serializer;
 
 /**
  * Hello world!
@@ -11,5 +12,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         new Gson();
+//        new Serializer();
     }
 }
